4b2960710c90b560af54040cc0bb87de9566ad3f
Qiwqiw-Alt <forgamefauzan10@gmail.com>
2026-05-09 07:33:03 +0700

    midahin delete dialog

--- app/src/main/java/com/example/tourtest/core/components/TourizmeDeleteDialog.kt
+++ app/src/main/java/com/example/tourtest/core/components/TourizmeDeleteDialog.kt
@@ -1,2 +1,34 @@
 package com.example.tourtest.core.components
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+@Composable
+public fun TourizmeDeleteDialog(
+    show: Boolean,
+    title: String = "Konfirmasi hapus",
+    message: String,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+){
+    if(show) {
+        AlertDialog(
+            onDismissRequest = onDismiss,
+            title = { Text(text = title) },
+            text = { Text(text = message) },
+            confirmButton = {
+                TextButton(onClick = onConfirm) {
+                    Text(text = "Hapus", color = MaterialTheme.colorScheme.error)
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = onDismiss) {
+                    Text(text =  "Batal")
+                }
+            }
+        )
+    }
+}
