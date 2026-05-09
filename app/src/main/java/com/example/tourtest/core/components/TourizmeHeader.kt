603a2d72fe501223619e7615dd77a186d8bdabdd
Qiwqiw-Alt <forgamefauzan10@gmail.com>
2026-05-09 07:35:03 +0700

    misahin header buat itinerary dan favoritepage

--- app/src/main/java/com/example/tourtest/core/components/TourizmeHeader.kt
+++ app/src/main/java/com/example/tourtest/core/components/TourizmeHeader.kt
@@ -1,2 +1,21 @@
 package com.example.tourtest.core.components
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+public fun TourizmeSimpleHeader(title: String) {
+    TopAppBar(
+        title = { Text(text = title, fontWeight = FontWeight.Bold) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    )
+}
