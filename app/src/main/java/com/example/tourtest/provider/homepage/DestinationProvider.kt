62170ffdc0d0900712a3014007ab65e49452dc7b
kristianws <181448915+kristianws@users.noreply.github.com>
2026-05-14 10:12:52 +0700

    provider for preview

--- app/src/main/java/com/example/tourtest/provider/homepage/DestinationProvider.kt
+++ app/src/main/java/com/example/tourtest/provider/homepage/DestinationProvider.kt
@@ -2,25 +2,27 @@ package com.example.tourtest.provider.homepage
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.example.tourtest.model.Destination
 
-class DestinationProvider : PreviewParameterProvider<Destination> {
+class DestinationProvider : PreviewParameterProvider<List<Destination>> {
     override val values = sequenceOf(
-        Destination(
-            id = "1",
-            name = "Keraton Surakarta",
-            location = "Solo",
-            description = "Tempat bersejarah yang sangat indah.",
-            price = "15.000",
-            imageUrl = "",
-            gmapUrl = "",
-        ),
-        Destination(
-            id = "2",
-            name = "Candi Cetho yang Sangat Jauh di Atas Gunung Karanganyar", // Contoh nama panjang
-            location = "Karanganyar",
-            description = "Pemandangannya luar biasa.",
-            price = "10.000",
-            imageUrl = "",
-            gmapUrl = ""
+        listOf(
+            Destination(
+                id = "1",
+                name = "Keraton Surakarta",
+                location = "Solo",
+                description = "Istana bersejarah...",
+                price = "15.000",
+                imageUrl = "",
+                gmapUrl = ""
+            ),
+            Destination(
+                id = "2",
+                name = "Pasar Gede",
+                location = "Solo",
+                description = "Pasar tradisional ikonik...",
+                price = "Gratis",
+                imageUrl = "",
+                gmapUrl = ""
+            )
         )
     )
 }
