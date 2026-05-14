bde47facaecba63049138e0f98952c85bc3e9306
Asxarthoz <177993558+Asxarthoz@users.noreply.github.com>
2026-05-14 10:04:22 +0700

    update global header

--- app/src/main/java/com/example/tourtest/core/components/TourizmeHeader.kt
+++ app/src/main/java/com/example/tourtest/core/components/TourizmeHeader.kt
@@ -1,19 +1,95 @@
 package com.example.tourtest.core.components
 
+import android.app.Notification
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-public fun TourizmeSimpleHeader(title: String) {
+public fun TourizmeSimpleHeader(
+    title: String,
+    searchQuery: String,
+    onSearchQueryChange: (String) -> Unit,
+    onNotificationClick: () -> Unit
+) {
+    var isSearchActive by remember { mutableStateOf(false) }
+
     TopAppBar(
-        title = { Text(text = title, fontWeight = FontWeight.Bold) },
+        title = {
+//            Text(text = title, fontWeight = FontWeight.Bold)
+            if (isSearchActive) {
+                TextField(
+                    value = searchQuery,
+                    onValueChange = onSearchQueryChange,
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = { Text(text = "Cari di $title...") },
+                    singleLine = true,
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    textStyle = MaterialTheme.typography.bodyLarge
+                )
+            } else {
+                Text(
+                    text = "Toruizme",
+                    fontWeight = FontWeight.ExtraBold,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
+                },
+        navigationIcon = {
+            if (isSearchActive) {
+                IconButton(onClick = {
+                    isSearchActive = false
+                    onSearchQueryChange("")
+                }) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Tutup" )
+                }
+            }
+        },
+        actions = {
+            if (!isSearchActive) {
+                IconButton(onClick = onNotificationClick) {
+                    Icon(Icons.Default.Notifications, contentDescription = "Notifikasi")
+                }
+
+                IconButton(onClick = { isSearchActive = true }) {
+                    Icon(Icons.Default.Search, contentDescription = "Cari")
+                }
+            } else {
+                if (searchQuery.isNotEmpty()) {
+                    IconButton(onClick = { onSearchQueryChange("") }) {
+                        Icon(Icons.Default.Close, contentDescription = "Hapus")
+                    }
+                }
+            }
+        },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
         )
