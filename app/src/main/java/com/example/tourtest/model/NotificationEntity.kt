1c8e8a97b7ac979a1040bcfc0c6f9deaed780918
Qiwqiw-Alt <forgamefauzan10@gmail.com>
2026-05-20 06:46:42 +0700

    add new variabel aka userId

--- app/src/main/java/com/example/tourtest/model/NotificationEntity.kt
+++ app/src/main/java/com/example/tourtest/model/NotificationEntity.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 data class NotificationEntity(
     @PrimaryKey
     val id: String,
+    val userId: String,
     val message: String,
     val destinationId: String,
     val timestamp: Long,
